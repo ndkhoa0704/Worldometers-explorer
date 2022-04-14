35421007a88280fa67f9addf3dd68951630933db
Khoa Nguyen Dang <ndkhoa0704@gmail.com>
2022-04-14 14:26:05 +0700

    Add combination

--- Tableau/combine.py
+++ Tableau/combine.py
@@ -1,25 +1,63 @@
 # Combine multiple data files
+
 import pandas as pd
-import numpy as np
 import re
 import os
 import datetime as dt
 
-def load(file_path):
-    files = os.listdir(file_path)
+def load(path: str):
+    files = os.listdir(path)
+    # Extract
+    dates = []
+    for f in files:
+        tmp = re.search(r'\d{4}-\d{1}[4]-\d{2}', f)
+        if tmp != None:
+            dates.append(dt.datetime.strptime(tmp.group(), r'%Y-%m-%d'))
+
     # Sort dates
-    dates = [dt.datetime.strptime(re.findall(r'\d{4}-[04]-\d{2}',f)[0],'%Y-%m-%d') for f in files]
     dates = sorted(dates,reverse=False)
     dates = [i.strftime('%Y-%m-%d') for i in dates]
     # Create a dict with keys are dates and values are data of those dates
-    dfs_raw = {d: pd.read_csv(f'{file_path}/worldometers-{d}.tsv',sep="\t") for d in dates}
-    print(dfs_raw.keys())
-# def combine():
+    dfs_raw = {d: pd.read_csv(f'{path}/worldometers-{d}.tsv',sep="\t") for d in dates}
+
+    return dfs_raw, dates
+
+def combine(raw: dict, dates: list, path: str):
+    # Countries
+    features = list(raw[dates[0]].columns)
+    countries = list(raw[dates[0]]['Country,Other'])
+    for c in countries:
+        country_df = pd.DataFrame(columns=['Date'] + features)
+        for d in dates:
+            row = pd.Series([d]).append(pd.Series(raw[d][raw[d]['Country,Other'] == c].iloc[0].to_list()))
+            row.index=['Date'] + features
+            row.name = 0
+            country_df = country_df.append(row, ignore_index=True)
+
+        country_df.to_csv(path + f'/Countries/{c}.tsv', sep='\t',index=False)
+    
+    # World
+    world = pd.DataFrame(columns=['Date'] + features[1:-1])
+    for d in dates:
+        row = pd.Series([d], index=['Date']).append(raw[d].iloc[:, 1:-1].sum(axis=0))
+        world = world.append(row, ignore_index=True)
+
+    world.to_csv(path + '/World.tsv', sep='\t', index=False)
+
+
+
+        
+    
     
 
+def save(path):
+    pass
+
 def main():
-    file_path = '../data'
-    load(file_path)
+    data_path = './data'
+    processed_path = './combined'
+    raw, dates = load(data_path)
+    combine(raw, dates, processed_path)
 
 if __name__=='__main__':
     main()
